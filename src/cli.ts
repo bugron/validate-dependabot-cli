baf21aed3e508ef43c54ce0722742e93b1abc175
Arsen Melikyan <bugron@mail.ru>
2024-09-01 23:02:06 +0400

    Update cli help text

--- src/cli.ts
+++ src/cli.ts
@@ -3,21 +3,26 @@ import meow from 'meow';
 export default meow(
     `
       Usage
-        $ npx @bugron/validate-dependabot-yaml <path>
-  
-      Path
-        Path to the dependabot configuration file relative to current working directory, .github/dependabot.yml by default
-      
+        $ npx @bugron/validate-dependabot-yaml [path]
+
+      [path]
+        Optional path to dependabot configuration file relative to current working directory (default: .github/dependabot.yml)
+
       Options
-        --format, -f  Logger type (json, markdown)
-        --pretty, -p  Only for json logger, prettify JSON output
+        --format, -f    Logger type, either json or markdown (default: markdown)
+        --pretty, -p    Only used for json logger, prettifies JSON output
 
       Output
         Success: process exits with status 0, no output is logged
         Failure: process exits with status 1, JSON or Markdown formatted validation error messages are logged
-  
+
       Examples
+        $ npx @bugron/validate-dependabot-yaml
+        $ npx @bugron/validate-dependabot-yaml config/dependabot.yaml
+        $ npx @bugron/validate-dependabot-yaml .github/dependabot.yml --f=json --p
         $ npx @bugron/validate-dependabot-yaml .github/dependabot.yml --format=json --pretty
+        $ npx @bugron/validate-dependabot-yaml .github/dependabot.yml --f=markdown
+        $ npx @bugron/validate-dependabot-yaml .github/dependabot.yml --format=markdown
   `,
     {
         importMeta: import.meta,
