830f2d7a4ffee4955f576f9adfa2b151e44f0fb5
Arsen Melikyan <bugron@mail.ru>
2024-09-01 06:01:03 +0400

    Format remaining files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/run.ts'],
-  format: ['esm'],
-  target: 'node20',
-  platform: 'node',
-  outDir: 'dist',
-  clean: true,
-  minify: true,
-  dts: false,
-  splitting: false
-})
+    entry: ['src/run.ts'],
+    format: ['esm'],
+    target: 'node20',
+    platform: 'node',
+    outDir: 'dist',
+    clean: true,
+    minify: true,
+    dts: false,
+    splitting: false,
+});
