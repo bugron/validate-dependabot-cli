710e1e6bf4c85faff3e2785de6f59b66d1352eda
Arsen Melikyan <bugron@mail.ru>
2024-09-01 06:54:13 +0400

    Fix the format option helper text, update usage

--- src/cli.ts
+++ src/cli.ts
@@ -3,13 +3,13 @@ import meow from 'meow';
 export default meow(
     `
       Usage
-        $ @bugron/validate-dependabot-yaml <path>
+        $ npx @bugron/validate-dependabot-yaml <path>
   
       Path
         Path to the dependabot configuration file relative to current working directory, .github/dependabot.yml by default
       
       Options
-        --logger, -l  Logger type (json, markdown)
+        --format, -f  Logger type (json, markdown)
         --pretty, -p  Only for json logger, prettify JSON output
 
       Output
@@ -17,7 +17,7 @@ export default meow(
         Failure: process exits with status 1, JSON or Markdown formatted validation error messages are logged
   
       Examples
-        $ @bugron/validate-dependabot-yaml .github/dependabot.yml --logger=json --pretty
+        $ npx @bugron/validate-dependabot-yaml .github/dependabot.yml --format=json --pretty
   `,
     {
         importMeta: import.meta,
